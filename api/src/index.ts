d4d391f12152515dfcc3a1bc9e703de1989375ed
jhollyfer <jhollyfer.fr@gmail.com>
2023-10-30 04:48:33 -0400

    initial setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,45 +1,73 @@
-import express from 'express';
-import chalk from 'chalk';
-import { Request, Response } from 'express';
-import figlet from 'figlet';
+import chalk from "chalk";
+import express, { Request, Response } from "express";
+import figlet from "figlet";
 
-await figlet('QoppaTech', (err, data) => {
-    if(err) {
-        console.log(chalk.yellow("CANNOT LOAD FIGLET ART"));
-    }
+import cors from "cors";
 
-    console.log(chalk.blue(data));
-})
-console.log(chalk.blue("Bem vindo ao CLI da API do teste de front-end da QoppaTech."));
-console.log(chalk.blue("Confira as informações em: https://github.com/QoppaTech/Qoppa-Front-End-Challenge/tree/main/api"));
-console.log("----------------------------------------------------------------")
+await figlet("QoppaTech", (err, data) => {
+  if (err) {
+    console.log(chalk.yellow("CANNOT LOAD FIGLET ART"));
+  }
 
+  console.log(chalk.blue(data));
+});
+console.log(
+  chalk.blue("Bem vindo ao CLI da API do teste de front-end da QoppaTech.")
+);
+console.log(
+  chalk.blue(
+    "Confira as informações em: https://github.com/QoppaTech/Qoppa-Front-End-Challenge/tree/main/api"
+  )
+);
+console.log("----------------------------------------------------------------");
 
 const app = express();
 
-app.use( express.json() );
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
 
 app
-.get("/", (req: Request, res: Response) => res.status(200).send("<h1>API DESAFIO: QoppaTech </h1>"))
-.post("/singUp", (req: Request, res: Response) => {
-    console.log(chalk.yellow("⏺ Requisição do tipo POST recebida na rota correta!"));
+  .get("/", (req: Request, res: Response) =>
+    res.status(200).send("<h1>API DESAFIO: QoppaTech </h1>")
+  )
+  .post("/singUp", (req: Request, res: Response) => {
+    console.log(
+      chalk.yellow("⏺ Requisição do tipo POST recebida na rota correta!")
+    );
 
     const bd = req.body;
 
-    if(bd.username, bd.email, bd.password) {
-        console.log(chalk.green("✅ Reconhecido! Parabéns!"))
-        return res.status(200).send();
+    if ((bd.username, bd.email, bd.password)) {
+      console.log(chalk.green("✅ Reconhecido! Parabéns!"));
+      return res.status(200).send();
     } else {
-        console.log(chalk.red("⛔️ Dados enviados incorretos ou não recebidos, confira os dados que devem ser recebidos em: https://github.com/QoppaTech/Qoppa-Front-End-Challenge/tree/main/api"));
-        return res.status(401).send(); 
+      console.log(
+        chalk.red(
+          "⛔️ Dados enviados incorretos ou não recebidos, confira os dados que devem ser recebidos em: https://github.com/QoppaTech/Qoppa-Front-End-Challenge/tree/main/api"
+        )
+      );
+      return res.status(401).send();
     }
-})
-.post("*", (req: Request, res: Response) => {
-    console.log(chalk.red("❌ Requisição do tipo POST recebida na rota incorreta!"));
+  })
+  .post("*", (req: Request, res: Response) => {
+    console.log(
+      chalk.red("❌ Requisição do tipo POST recebida na rota incorreta!")
+    );
     return res.status(404).send();
-})
-.get("*", (req: Request, res: Response) => {
-    return res.status(404).send("<h1>API DESAFIO: 404 -> PÁGINA NÃO ENCONTRADA -> quoppaTech </h1>")
-})
+  })
+  .get("*", (req: Request, res: Response) => {
+    return res
+      .status(404)
+      .send(
+        "<h1>API DESAFIO: 404 -> PÁGINA NÃO ENCONTRADA -> quoppaTech </h1>"
+      );
+  });
 
-app.listen(3000, () => console.log(chalk.green(`Qoppa_Desafio: Rodando na porta 3000`)));
+app.listen(3000, () =>
+  console.log(chalk.green(`Qoppa_Desafio: Rodando na porta 3000`))
+);
